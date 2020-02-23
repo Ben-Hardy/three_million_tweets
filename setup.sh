c72e9d5cb6beee8a1075ba4eef9d54e7721062fe
Ben <ben@benjamin.zone>
2020-02-23 12:28:27 -0600

    Completed Conclusion
    I may still add more to this analysis, but I wanted to conclude it in some fashion rather than leave it open-ended, so I did that.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+# run this script in the directory you want to work in. If you have already set up your virtual environment, then comment out the virtual environment section
+# if you already have the data, then simply move all of the .csv files into a directory called tweets and the programs should work as expected.
 virtualenv venv -p python3
 source venv/bin/activate
 pip install pandas
@@ -11,14 +12,15 @@ python -m textblob.download_corpora
 pip install scipy
 pip install spacy
 pip install jupyter
+pip install seaborn
 pip install emoji
 
-# mkdir tweets
-#mkdir csv
+mkdir tweets
+mkdir csv
 
-#git clone https://github.com/fivethirtyeight/russian-troll-tweets.git
-#cp ./russian-troll-tweets/* ./tweets/
-#rm -rf ./russian-troll-tweets
-#rm ./tweets/README.md
+git clone https://github.com/fivethirtyeight/russian-troll-tweets.git
+cp ./russian-troll-tweets/* ./tweets/
+rm -rf ./russian-troll-tweets
+rm ./tweets/README.md
 
 
